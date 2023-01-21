31678839d0943592025ae22a6f180550a4f8bde8
Jephte Dunia <jephtedunia07@gmail.com>
2023-01-21 13:26:37 +0200

    uupdate view and urls

--- todobackend/api/views.py
+++ todobackend/api/views.py
@@ -3,11 +3,16 @@ from .serializers import TodoSerializer
 from todo.models import Todo
 # Create your views here.
 
-class TodoList(generics.ListAPIView):
-
-
+class TodoListCreate(generics.ListCreateAPIView):
+    # ListAPIView needs two attributes, serializer_class and queryset
+    # We specify TodoSerializer which we earlier implemented
     serializer_class = TodoSerializer
+
     def get_queryset(self):
         user =  self.request.user
         return Todo.objects.filter(user=user).order_by('-created')
+
+    def perform_create(self, serializer):
+        # Serializer holds a django model
+        serializer.save(user=self.request.user)
     
